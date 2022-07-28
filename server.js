921429fede918d5cbdee23f721289768810e396e
TestAccountZaVjezbe <ankoratest123@gmail.com>
2022-07-28 21:14:53 +0200

    feat: [IME_NA_GIT] setup router and controller

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import testRouter from './routers/testRouter'
 
 const server = express();
 
@@ -10,6 +11,7 @@ const init = () => {
     server.use(express.json());
     server.use(cors());
     
+    server.use('/test', testRouter)
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`)
     })
