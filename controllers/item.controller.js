dc6f3351f3db8c41193aa98dba4cf2e4c6aee9ab
TestAccountZaVjezbe <ankoratest123@gmail.com>
2022-08-09 20:18:20 +0200

    fix: [name] remove unused function

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,15 +1,5 @@
 import itemServiceHandler from "../services/item.service";
 
-const getOneCategory = async(req, res) => {
-    try{
-        const {params} = req;
-        const category = await categoryServiceHandler.getOneCategory(params.id);
-        return res.send(category);
-    } catch(e){
-        return res.send({ msg: "Internal server error"}).status(500);
-    }
-};
-
 const getOneItem = async(req, res) => {
     try{
         const {params} = req;
